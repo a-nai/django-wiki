34ff8fda15fc737d005063a3b13a061757ccc61e
a-nai <aeremchuk@facebook.com>
2015-04-07 13:13:39 +0300

    Update models.py

--- wiki/plugins/images/models.py
+++ wiki/plugins/images/models.py
@@ -82,7 +82,7 @@ class ImageRevision(RevisionPluginRevision):
             try:
                 #path1=self.image.path.split("/")[:-1]
                 #path1=.split("/")
-                return self.image.path[17:]
+                return self.image.path[25:]
             except OSError:
                 pass
         return None
